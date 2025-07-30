1f5036310ffb3018f84ab5f370f2ece8cfdb9b74
Nneka <777uloma777@gmail.com>
2025-07-29 22:46:26 -0400

    moved MONGODB connection to config/db.js and updated server.js

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 // I’m importing all my user-related routes from the routes folder so I can use them in this main server file
 import userRoutes from './routes/userRoutes.js';
-
-
+//We're grabbing connectDB function from config/db.js file so it can be used here
+import connectDB from './config/db.js';
 
 // Run the dotenv config so my app can access values from .env
 dotenv.config();
 
 // Create the Express app — this will be my backend server
 const app = express();
+
+connectDB();
 
 // Use middleware to tell Express to automatically parse JSON from requests
 app.use(express.json());
@@ -24,8 +26,8 @@ app.use('/api/users', userRoutes);
 // Connect to MongoDB using the URI I stored in .env
 // If successful, log a message. If it fails, catch and show the error.
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 // Choose which port the server should run on (use env first, or default to 3000)
 const PORT = process.env.PORT || 3000;
